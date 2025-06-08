4f623949472aef0e6604078ff44575c525652d65
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2025-06-09 00:33:27 +0200

    feat: replaces PHPMailer by Symfony/Mailer in Main

--- PHP/src/Main.php
+++ PHP/src/Main.php
@@ -3,12 +3,14 @@
 namespace BirthdayGreetings;
 
 use BirthdayGreetings\Adapters\CsvEmployeeRepository;
-use BirthdayGreetings\Adapters\Emails\PHPMailerMessageSender;
+use BirthdayGreetings\Adapters\Emails\SymfonyMailerMessageSender;
 use BirthdayGreetings\Emails\BirthdayGreetingEmailMessageComposer;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Logger;
 use Rx\Scheduler;
 use Rx\SchedulerInterface;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -23,7 +25,10 @@ final class Main
 
         $service = new BirthdayService(
             new CsvEmployeeRepository('employee_data.txt'),
-            new PHPMailerMessageSender(new BirthdayGreetingEmailMessageComposer(), 'localhost', 25),
+            new SymfonyMailerMessageSender(
+                new BirthdayGreetingEmailMessageComposer(),
+                new Mailer(Transport::fromDsn('smtp://localhost:25'))
+            ),
             new Logger('BirthdayGreetings', [new ErrorLogHandler()])
         );
         $service->sendGreetings((new XDate()));
